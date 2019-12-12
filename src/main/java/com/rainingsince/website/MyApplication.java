003c76c2de7c56ec99b740dd4ab94f3ee639662e
曹德博 <13335137159@163.com>
2019-12-12 16:25:18 +0800

    打包方式 修改

--- src/main/java/com/rainingsince/website/MyApplication.java
+++ src/main/java/com/rainingsince/website/MyApplication.java
@@ -1,21 +1,12 @@
 package com.rainingsince.website;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyApplication extends SpringBootServletInitializer {
+public class MyApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MyApplication.class);
     }
 }
